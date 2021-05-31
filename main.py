5479da393ff2af8f807111e0aff3f074dd5b03e7
MaximDragun <67150875+MaximDragun@users.noreply.github.com>
2021-05-31 12:52:08 +0300

    Ограничение по грунту до 6000 точек, меняем алгоритм нормализации и увеличиваем кол-во эпох до 2500

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ warnings.simplefilter(action='ignore', category=DeprecationWarning)
 # 6 — здания (крыши) (Building)
 # 7 — ложно отраженные точки (Low point)
 # 8 — Model keypoints
-# 9 — Other points
+# 9 — Other points.
 
 # columns = ["res", "X", "Y", "Z"]
 
